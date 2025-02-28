5972f77c7b906afa8ca2e52c6fac8bdb841682b4
NicolasDuqueA123 <nicolas.duque1821@gmail.com>
2025-02-27 23:43:40 -0500

    commit veinte seis

--- app.py
+++ app.py
@@ -26,15 +26,15 @@ with center_col:
 
     st.markdown("<h1 style='text-align: center;'>Informe sobre la Transición Energética</h1>", unsafe_allow_html=True)
     st.markdown("<h5 style='text-align: left;'>Objetivo General: </h5>", unsafe_allow_html=True)
-    st.markdown("<h7 style='text-align: justify;'>Identificar el comportamiento de la generación eléctrica de siete diferentes fuentes de energía en Colombia para el periodo 2006-2023.'</h7>", unsafe_allow_html=True)
+    st.markdown("<h7 style='text-align: justify;'>Identificar el comportamiento de la generación eléctrica de siete diferentes fuentes de energía en Colombia para el periodo 2006-2023.</h7>", unsafe_allow_html=True)
     st.markdown("<h5 style='text-align: left;'>Objetivos especificos: </h5>", unsafe_allow_html=True)
-    st.markdown("<h7 style='text-align: justify;'> - Visualizar los indicadores descriptivos de las fuentes de energía definidas para el estudio en Colombia en el periodo de investigación.'</h7>", unsafe_allow_html=True)
-    st.markdown("<h7 style='text-align: justify;'> - Identificar el grado de correlación de las fuentes de energía definidas para el estudio en Colombia en el periodo de investigación'</h7>", unsafe_allow_html=True)
-    st.markdown("<h7 style='text-align: justify;'> - Comparar la evolución de los porcentajes de generación eléctrica de lostipos de energía definidas para el estudio en Colombia en el periodo de investigación'</h7>", unsafe_allow_html=True)
-    st.markdown("<h7 style='text-align: justify;'> - Comparar la evolución de la generación eléctrica de los tipos de energía definidas para el estudio en Colombia en el periodo de investigación.'</h7>", unsafe_allow_html=True)
+    st.markdown("<h7 style='text-align: justify;'> - Visualizar los indicadores descriptivos de las fuentes de energía definidas para el estudio en Colombia en el periodo de investigación.</h7>", unsafe_allow_html=True)
+    st.markdown("<h7 style='text-align: justify;'> - Identificar el grado de correlación de las fuentes de energía definidas para el estudio en Colombia en el periodo de investigación</h7>", unsafe_allow_html=True)
+    st.markdown("<h7 style='text-align: justify;'> - Comparar la evolución de los porcentajes de generación eléctrica de lostipos de energía definidas para el estudio en Colombia en el periodo de investigación</h7>", unsafe_allow_html=True)
+    st.markdown("<h7 style='text-align: justify;'> - Comparar la evolución de la generación eléctrica de los tipos de energía definidas para el estudio en Colombia en el periodo de investigación.</h7>", unsafe_allow_html=True)
 
 
-    imagen5 = Image.open("equipo.jpeg")
+    imagen5 = Image.open("media/equipo.jpeg")
     st.image(imagen5, use_container_width=False, width=900,
-             caption="León Bello - Edwin Duqeu - Nicólas Duque - Dayan Gaviria"
+             caption="León Bello - Edwin Duque - Nicólas Duque - Dayan Gaviria"
              )
