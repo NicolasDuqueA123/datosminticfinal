95fa7951f8c8fdc14b079eb77793dcd3b43f4dcf
NicolasDuqueA123 <nicolas.duque1821@gmail.com>
2025-02-28 09:13:59 -0500

    commit 40

--- pages/adicionales.py
+++ pages/adicionales.py
@@ -9,9 +9,7 @@ import matplotlib.ticker as ticker
 import streamlit as st
 import utilidades as util
 from PIL import Image
-import geopandas as gpd
-from matplotlib.lines import Line2D
-from matplotlib.patches import Patch
+
 
 st.set_page_config(
     page_title='Others',
@@ -171,67 +169,8 @@ st.pyplot(plt)
 # Mapamundi
 st.markdown("<h5 style='text-align: center;'>2. Mapamundi de generación renovable</h5>", unsafe_allow_html=True)
 st.markdown("<h7 style='text-align: center;'>Se visualiza cuales son los paises que generan más energia de fuentes renovables a medida que el tono del color se oscurece</h7>", unsafe_allow_html=True)
-#mundo = Image.open("media/mapamundi.jpeg")
-#st.image(mundo, use_container_width=False, width=700, caption=" ")
-
-data_energy = pd.read_csv("data/owid_energy_data.csv", index_col=None)
-# Cargar el Shapefile con GeoPandas
-world = gpd.read_file("data/ne_50m_admin_0_countries.shp")
-# Estandarizar los nombres de los países en el Shapefile (mayúsculas y sin espacios
-world["ADMIN"] = world["ADMIN"].str.upper().str.strip()
-# Generar Mapa base del mundo
-world.plot(figsize=(15, 13), edgecolor='black', facecolor='none')
-plt.title('Mapa base del mundo', fontsize=14)
-plt.axis('off')
-data_energy_2023 = data_energy[data_energy['year'] == 2023]
-data_energy_2023 = data_energy_2023[['iso_code', 'renewables_share_elec']]
-#.filter(regex="iso_code|renewables_share_elec", axis=1)
-
-#convierto la la columna world['ISO_A3'] en una lista
-iso_codes = world['ISO_A3'].tolist()
-
-# Seleccionamos aquellos paises que estan en la lista creada
-data_energy_2023 = data_energy_2023[data_energy_2023['iso_code'].isin(iso_codes)]
-data_energy_2023.reset_index(drop=True, inplace=True)
-total           = data_energy_2023.isnull().sum().sort_values(ascending=False)
-percent         = (data_energy_2023.isnull().sum()/data_energy_2023.isnull().count()*100).sort_values(ascending=False)
-missing_train1  = pd.concat([total,percent],axis=1,keys=["Total","Percent"])
-world_filt = world[['ADMIN', 'ISO_A3', 'geometry']]
-
-# Seleccionamos aquellos paises que estan en la lista creada
-world_filt.reset_index(drop=True, inplace=True)
-world_filt = world_filt.rename(columns={'ISO_A3': 'iso_code'})
-world_energy = world_filt.merge(data_energy_2023, on='iso_code', how='left')
-# Cambiar el color de fondo del área de dibujo
-ax.set_facecolor("lightgray")  # Fondo del mapa
-plt.gca().set_facecolor("lightgray")  # Asegurar que se aplique en toda la zona de gráficos
-fig.patch.set_facecolor("whitesmoke")  # Fondo exterior de la figura
-
-# Graficar el DataFrame GeoPandas
-world_energy.plot(column='renewables_share_elec',
-                  cmap='Blues',
-                  linewidth=0.8,
-                  ax=ax,
-                  edgecolor='black',  # Se mantiene un solo edgecolor
-                  legend=True,
-                  legend_kwds={'label': "proporción de energía renovable",
-                               'orientation': "vertical",
-                               'shrink': 0.4,   # Ajusta el tamaño de la barra de colores (1 = tamaño normal)
-                               'aspect': 20},
-                  missing_kwds={"color": "purple", "edgecolor": "black"}  # Para áreas sin datos
-                  )
-
-# Agregar leyenda para zonas sin datos
-legend_elements = [Patch(facecolor='purple', edgecolor='black', label='Sin datos')]
-ax.legend(handles=legend_elements, loc='lower left')
-
-# Título y eliminación de ejes
-ax.set_title('Porcentaje de energía renovable - 2023', fontsize=16)
-ax.axis('off')
-
-# Mostrar el gráfico
-st.pyplot(plt)
-
+mundo = Image.open("media/mapamundi.jpeg")
+st.image(mundo, use_container_width=False, width=700, caption=" ")
 
 
 # LINKS A COLAB
